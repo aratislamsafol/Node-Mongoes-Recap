3901c2589e91cbb6ccd7b7a82c0958457f219039
aratislamsafol <aratislamsafol@gmail.com>
2025-07-12 15:51:18 +0600

    write file

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ const fs= require('fs');
 
 const server = http.createServer(function (req, res) {
   if(req.url =="/") {
-        fs.readFile('Home.html', function(err, data) {
-            res.writeHead(200, {"content-type": "text/html"});
-            res.write(data);
-            res.end();
+        fs.writeFile('demo.text', "hello Kacher Manushjon", (err)=>{
+            if(err) {
+                res.writeHead(200, {'content-type': 'text/html'})
+                res.write('File Write Fail');
+                res.end()
+            } else {
+                res.writeHead(200, {'content-type': 'text/html'})
+                res.write('File Success');
+                res.end()
+            }
         })
   }
 })
